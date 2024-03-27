9b9b3b0a6ff8bedd9dd628cd494ceb1d9040a72b
kiwiidb <kwinten.debacker@hotmail.com>
2024-03-27 16:39:41 +0100

    more minimalistic

--- lib/views/game.dart
+++ lib/views/game.dart
@@ -33,91 +33,81 @@ Widget buildPlayerButtons(int index) {
     leftDelta = 1;
     rightDelta = -1;
   }
-  return Container(
-    margin: const EdgeInsets.all(3),
-    decoration: BoxDecoration(
-        color: Colors.grey.withOpacity(0.5),
-        borderRadius: BorderRadius.circular(20)),
-    child: Center(
-      child: Column(
-        children: [
-          Transform.rotate(
-            angle: angle,
-            child: SizedBox(
-                height: 200,
-                width: 200,
-                child: TextButton(
-                  onPressed: () {
-                    ctrl.pickedPlayerIndex = index;
-                    Get.to(const PickPlayerScreen());
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.network(ctrl.players[index].profileImageUrl,
-                        fit: BoxFit.cover, errorBuilder: (BuildContext context,
-                            Object exception, StackTrace? stackTrace) {
-                      return const Icon(
-                        Icons.person,
-                        size: 75,
-                        color: Colors.black,
-                      );
-                    }),
-                  ),
-                )),
-          ),
-          Stack(
-            children: [
-              SizedBox(
-                height: 150,
-                width: 200,
-                child: GetX<GameController>(builder: (ctrl) {
-                  var lifeTotal = ctrl.players[index].lifeTotal;
-                  return Center(
-                    child: Transform.rotate(
-                      angle: angle,
-                      child: Text(
-                        '$lifeTotal',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 80.0,
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-              ),
-              SizedBox(
-                height: 150,
-                width: 200,
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: SizedBox.expand(
-                        child: TextButton(
-                          onPressed: () {
-                            ctrl.updatePlayer(index, leftDelta);
-                          },
-                          child: Container(),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: SizedBox.expand(
-                        child: TextButton(
-                          onPressed: () {
-                            ctrl.updatePlayer(index, rightDelta);
-                          },
-                          child: Container(),
-                        ),
-                      ),
-                    ),
-                  ],
+  return Center(
+    child: Column(
+      children: [
+        Transform.rotate(
+          angle: angle,
+          child: SizedBox(
+              height: 200,
+              width: 200,
+              child: TextButton(
+                onPressed: () {
+                  ctrl.pickedPlayerIndex = index;
+                  Get.to(const PickPlayerScreen());
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(ctrl.players[index].profileImageUrl,
+                      fit: BoxFit.cover, errorBuilder: (BuildContext context,
+                          Object exception, StackTrace? stackTrace) {
+                    return const Icon(
+                      Icons.person,
+                      size: 75,
+                      color: Colors.black,
+                    );
+                  }),
                 ),
-              )
-            ],
-          ),
-        ],
-      ),
+              )),
+        ),
+        Stack(
+          children: [
+            GetX<GameController>(builder: (ctrl) {
+              var lifeTotal = ctrl.players[index].lifeTotal;
+              return Center(
+                child: Transform.rotate(
+                  angle: angle,
+                  child: Text(
+                    '$lifeTotal',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 80.0,
+                    ),
+                  ),
+                ),
+              );
+            }),
+            SizedBox(
+              height: 150,
+              width: 200,
+              child: Row(
+                children: [
+                  Flexible(
+                    child: SizedBox.expand(
+                      child: TextButton(
+                        onPressed: () {
+                          ctrl.updatePlayer(index, leftDelta);
+                        },
+                        child: Container(),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: SizedBox.expand(
+                      child: TextButton(
+                        onPressed: () {
+                          ctrl.updatePlayer(index, rightDelta);
+                        },
+                        child: Container(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
     ),
   );
 }
